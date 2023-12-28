98b8b2dd265aa005a262fa4e951ac2bff4123d14
candykarthi002 <78299841+candykarthi002@users.noreply.github.com>
2023-12-28 20:18:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://blogsphere-app.netlify.app", credentials: true }));
 
 // Routes Middleware
 app.use("/api/users", userRoute);
