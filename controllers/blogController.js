d4e64d2153bf1444d5e77b9a9d71575b087aca58
candykarthi002 <78299841+candykarthi002@users.noreply.github.com>
2024-01-07 23:15:48 +0530

    Update blogController.js

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 const Blog = require("../models/blogs");
 const BlogUser = require("../models/blogUser");
 
@@ -9,6 +10,51 @@ const getBlogs = asyncHandler(async (req, res) => {
   } else {
     res.status(404).json({ blogs: "No Blogs Found" });
   }
+});
+
+const getBlogById = asyncHandler(async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+  if (blog) {
+    res.status(200).json({ blog: blog });
+  } else {
+    res.status(404).json({ blog: "No Blogs Found" });
+  }
+});
+
+const likeBlog = asyncHandler(async (req, res) => {
+  const user = jwt.verify(await req.cookies.jwt_token, process.env.JWT_SECRET);
+  const blog = await Blog.findById(req.params.id);
+  if (blog) {
+    if (!blog.likes.includes(user.id)) {
+      blog.likes.push(user.id);
+      blog.save();
+    } else {
+      blog.likes.splice(blog.likes.indexOf(user.id), 1);
+      blog.save();
+    }
+    console.log(blog.likes);
+    res.status(200).json(blog);
+  } else {
+    res.status(400).json({ msg: "Error updating like" });
+  }
+  // console.log(jwt.verify(await req.cookies.jwt_token, process.env.JWT_SECRET));
+  // console.log(req.params.id);
+});
+
+const commentBlog = asyncHandler(async (req, res) => {
+  const user = jwt.verify(await req.cookies.jwt_token, process.env.JWT_SECRET);
+  const blog = await Blog.findById(req.params.id);
+  const { name } = await BlogUser.findById(user.id);
+  const { comment } = req.body;
+  if (blog) {
+    blog.comments.push({ user: user.id, username: name, content: comment });
+    blog.save();
+    res.status(200).json(blog);
+  } else {
+    res.status(400).json({ msg: "Error updating like" });
+  }
+  // console.log(jwt.verify(await req.cookies.jwt_token, process.env.JWT_SECRET));
+  // console.log(req.params.id);
 });
 
 const addBlog = asyncHandler(async (req, res) => {
@@ -39,4 +85,7 @@ const addBlog = asyncHandler(async (req, res) => {
 module.exports = {
   getBlogs,
   addBlog,
+  likeBlog,
+  getBlogById,
+  commentBlog,
 };
