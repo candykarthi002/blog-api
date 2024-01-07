9149e8e8fbc1a0463d1dc92548ed78c04c9b9987
candykarthi002 <78299841+candykarthi002@users.noreply.github.com>
2024-01-07 23:17:00 +0530

    Update blogRoute.js

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { getBlogs, addBlog } = require("../controllers/blogController");
+const {
+  getBlogs,
+  addBlog,
+  likeBlog,
+  getBlogById,
+  commentBlog,
+} = require("../controllers/blogController");
 const protect = require("../middlewares/authMiddleware");
 
 router.post("/addblog", protect, addBlog);
 router.get("/getblogs", getBlogs);
+router.get("/getblog/:id", getBlogById);
+router.put("/like/:id", protect, likeBlog);
+router.put("/comment/:id", protect, commentBlog);
 
 module.exports = router;
