3864dfbcfa9c7b263b0e19540ea620f0411ff6a0
candykarthi002 <78299841+candykarthi002@users.noreply.github.com>
2024-01-19 13:43:15 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -85,7 +85,7 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(200).json({
       msg: "User Found",
       token: token,
-      username: user.name,
+      username: user,
     });
   } else {
     res.status(400);
